15dfa06cba37242f8166c12d01b4a87faa6d8efd
frshock <will.s.crane@gmail.com>
2016-12-01 18:06:46 -0500

    UX

--- steam-api-front/src/components/Register.js
+++ steam-api-front/src/components/Register.js
@@ -18,8 +18,8 @@ class Register extends Component {
       .post('/encrypt', this.state)
       .then((res) => {
         console.log(res);
-        window.location = '/login';
       })
+    window.location = '/login';
   }
 
   txtFieldChange(e) {
