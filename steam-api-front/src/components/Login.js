4252228002f7e608a5213b1738eb5266806c5f4e
frshock <will.s.crane@gmail.com>
2016-12-12 12:54:22 -0500

    Aligning background

--- steam-api-front/src/components/Login.js
+++ steam-api-front/src/components/Login.js
@@ -58,6 +58,7 @@ class Login extends Component {
     let style = {
       background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("https://ubistatic-a.akamaihd.net/0021/community-intelligence/prod/article/56784c5484c3d.jpg")',
       backgroundColor: '#FFFFFF',
+      backgroundPositionY: '-100px',
       backgroundSize: 'cover',
       color: '#FFFFFF'
     }
